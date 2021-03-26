7f452f7434b26398a816d678b5c0a7eae0ceb7d8
hungdhm-0574 <do.hoang.minh.hung@sun-asterisk.com>
2021-03-26 11:18:00 +0700

    test git hub ver2 without CD

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -34,6 +34,7 @@ data "aws_iam_policy_document" "pipeline" {
       "s3:GetObject",
       "s3:ListBucket",
       "s3:PutObject",
+      "s3:PutObjectAcl"
     ]
 
     resources = [
@@ -328,7 +329,7 @@ resource "aws_codedeploy_deployment_group" "this" {
 
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 5
+      termination_wait_time_in_minutes = 0
     }
   }
 
